6ca6cd25b45a73760184ed3757275d39a1c13700
Kamal ElFadl <kamal.elfald@gmail.com>
2022-11-25 10:21:58 +0700

    add search tv test

--- search/test/domain/usecase/search_tv_test.dart
+++ search/test/domain/usecase/search_tv_test.dart
@@ -1,35 +1,39 @@
 import 'package:core/domain/entities/movie.dart';
+import 'package:core/domain/entities/tv.dart';
 import 'package:core/domain/repositories/movie_repository.dart';
+import 'package:core/domain/repositories/tv_repository.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
 import 'package:search/domain/usecase/search_movies.dart';
+import 'package:search/domain/usecase/search_tv.dart';
 import 'search_movies_test.mocks.dart';
+import 'search_tv_test.mocks.dart';
 
 @GenerateMocks([
-  MovieRepository,
+  TvRepository,
 ])
 void main() {
-  late SearchMovies usecase;
-  late MockMovieRepository mockMovieRepository;
+  late SearchTv usecase;
+  late MockTvRepository mockTvRepository;
 
   setUp(() {
-    mockMovieRepository = MockMovieRepository();
-    usecase = SearchMovies(mockMovieRepository);
+    mockTvRepository = MockTvRepository();
+    usecase = SearchTv(mockTvRepository);
   });
 
-  final tMovies = <Movie>[];
-  final tQuery = 'Spiderman';
+  final tTvs = <Tv>[];
+  final tQuery = 'hulk';
 
   test('should get list of movies from the repository', () async {
     // arrange
-    when(mockMovieRepository.searchMovies(tQuery))
-        .thenAnswer((_) async => Right(tMovies));
+    when(mockTvRepository.searchTvs(tQuery))
+        .thenAnswer((_) async => Right(tTvs));
     // act
     final result = await usecase.execute(tQuery);
     // assert
-    expect(result, Right(tMovies));
+    expect(result, Right(tTvs));
   });
 }
